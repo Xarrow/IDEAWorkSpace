1ca68a0091754524d791cfd4c17014f9ef88aabe
zhangjian5 <zhangjian12424@gmail.com>
2015-12-03 14:24:39 +0800

    init

---
+++ src/com/tuniu/code/day1/Init.java
@@ -0,0 +1,11 @@
+package com.tuniu.code.day1;
+
+/**
+ * Created by zhangjian5 on 2015/12/3.
+ */
+public class Init {
+    public static  void  main (String []args)
+    {
+        System.out.print("Hello World");
+    }
+}
